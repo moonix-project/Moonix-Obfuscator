9f03d25bd4a38f427996d23df2fb0265a05374d1
Moonix <satyros495@gmail.com>
2024-12-12 12:04:09 -0300

    Add files via upload

--- input.lua
+++ input.lua
@@ -2,3 +2,6 @@ print(1 + 1)
 print(2 * 3)
 print(10 / 2)
 print(5 - 3)
+print("seems that shits working")
+local a = "hello world"
+print(a)
--- obf_module.lua
+++ obf_module.lua
@@ -5,9 +5,7 @@ local function generate_complex_name(length)
     local chars = {}
     local char_sets = {{48, 57}, {65, 90}, {97, 122}, {95, 95}}
 
-    chars[1] = string.char(math.random(97, 122))
-
-    for i = 2, length do
+    for i = 1, length do
         local set = char_sets[math.random(#char_sets)]
         chars[i] = string.char(math.random(set[1], set[2]))
     end
@@ -17,11 +15,21 @@ end
 
 local function encrypt_arithmetic(code)
     local operations = {
-        ["+"] = function(a, b) return a + b end,
-        ["-"] = function(a, b) return a - b end,
-        ["*"] = function(a, b) return a * b end,
-        ["/"] = function(a, b) return a / b end,
-        ["%"] = function(a, b) return a % b end
+        ["+"] = function(a, b)
+            return a + b
+        end,
+        ["-"] = function(a, b)
+            return a - b
+        end,
+        ["*"] = function(a, b)
+            return a * b
+        end,
+        ["/"] = function(a, b)
+            return a / b
+        end,
+        ["%"] = function(a, b)
+            return a % b
+        end
     }
 
     local function generate_math_wrapper()
@@ -48,8 +56,30 @@ local function encrypt_arithmetic(code)
         ["%"] = op_names[5]
     }
 
-    code = code:gsub("(%d+)%s*([%+%-%*%/%%])%s*(%d+)", function(a, op, b)
-        local wrapped_op = string.format("%s['%s'](%s,%s)", math_wrapper_name, op_map[op], a, b)
+    local function encrypt_number(num)
+        local num_str = tostring(num)
+        local encrypted = {}
+        local key = math.random(1, 255)
+        for i = 1, #num_str do
+            encrypted[i] = string.format("%d", num_str:byte(i) ~ key)
+        end
+        return string.format("({%s},%d)", table.concat(encrypted, ","), key)
+    end
+
+    local function decode_number_func()
+        local decode_func_name = generate_complex_name()
+        return string.format(
+            [[local function %s(t,k)local r="" for i,v in ipairs(t) do r=r..string.char(v~k) end return tonumber(r) end
+]], decode_func_name), decode_func_name
+    end
+
+    local decode_num_code, decode_num_name = decode_number_func()
+
+    code = decode_num_code .. code
+
+    code = code:gsub("([%d%.]+)%s*([%+%-%*%/%%])%s*([%d%.]+)", function(a, op, b)
+        local wrapped_op = string.format("%s['%s'](%s,%s)", math_wrapper_name, op_map[op],
+            decode_num_name .. encrypt_number(a), decode_num_name .. encrypt_number(b))
         return wrapped_op
     end)
 
@@ -66,9 +96,17 @@ local function encrypt_function_calls(code)
     local string_format_name = generate_complex_name()
     local string_byte_name = generate_complex_name()
     local string_gsub_name = generate_complex_name()
+    local os_time_name = generate_complex_name()
+    local math_floor_name = generate_complex_name()
+    local math_abs_name = generate_complex_name()
+    local string_len_name = generate_complex_name()
+    local string_sub_name = generate_complex_name()
 
-    local wrapper_code = string.format([[local %s={["%s"]=print,["%s"]=math.random,["%s"]=string.char,["%s"]=table.insert,["%s"]=table.concat,["%s"]=string.format,["%s"]=string.byte,["%s"]=string.gsub}
-]], function_wrapper_name, print_func_name, math_random_name, string_char_name, table_insert_name, table_concat_name, string_format_name, string_byte_name, string_gsub_name)
+    local wrapper_code = string.format(
+        [[local %s={["%s"]=print,["%s"]=math.random,["%s"]=string.char,["%s"]=table.insert,["%s"]=table.concat,["%s"]=string.format,["%s"]=string.byte,["%s"]=string.gsub,["%s"]=os.time,["%s"]=math.floor,["%s"]=math.abs,["%s"]=string.len,["%s"]=string.sub}
+]], function_wrapper_name, print_func_name, math_random_name, string_char_name, table_insert_name, table_concat_name,
+        string_format_name, string_byte_name, string_gsub_name, os_time_name, math_floor_name, math_abs_name,
+        string_len_name, string_sub_name)
 
     code = code:gsub("(%w+)(%s*%)?)%s*%(", function(func_name, trailing)
         local obfuscated_func = function_wrapper_name .. "['"
@@ -88,6 +126,16 @@ local function encrypt_function_calls(code)
             obfuscated_func = obfuscated_func .. string_byte_name
         elseif func_name == "string.gsub" then
             obfuscated_func = obfuscated_func .. string_gsub_name
+        elseif func_name == "os.time" then
+            obfuscated_func = obfuscated_func .. os_time_name
+        elseif func_name == "math.floor" then
+            obfuscated_func = obfuscated_func .. math_floor_name
+        elseif func_name == "math.abs" then
+            obfuscated_func = obfuscated_func .. math_abs_name
+        elseif func_name == "string.len" then
+            obfuscated_func = obfuscated_func .. string_len_name
+        elseif func_name == "string.sub" then
+            obfuscated_func = obfuscated_func .. string_sub_name
         else
             return func_name .. trailing .. "("
         end
@@ -138,12 +186,85 @@ local function add_junk_code(code)
         local junk_value2 = math.random(100000, 999999)
         table.insert(junk_lines, string.format("local %s = %d %s %d", junk_var_name, junk_value, junk_op, junk_value2))
         local junk_if_var = generate_complex_name()
-        table.insert(junk_lines, string.format("local %s = %d", junk_if_var, math.random(0,1)))
-        table.insert(junk_lines, string.format("if %s == 1 then %s = %d end", junk_if_var, junk_var_name, math.random(100000, 999999)))
+        table.insert(junk_lines, string.format("local %s = %d", junk_if_var, math.random(0, 1)))
+        table.insert(junk_lines, string.format("if %s == 1 then %s = %d end", junk_if_var, junk_var_name,
+            math.random(100000, 999999)))
     end
     return table.concat(junk_lines, "\n") .. "\n" .. code
 end
 
+local function virtualize_code(code)
+    local vm_name = generate_complex_name()
+    local instructions = {}
+    local instruction_map = {}
+
+    local function add_instruction(name, func)
+        local instruction_id = #instructions + 1
+        instructions[instruction_id] = func
+        instruction_map[name] = instruction_id
+    end
+
+    add_instruction("ADD", function(a, b)
+        return a + b
+    end)
+    add_instruction("SUB", function(a, b)
+        return a - b
+    end)
+    add_instruction("MUL", function(a, b)
+        return a * b
+    end)
+    add_instruction("DIV", function(a, b)
+        return a / b
+    end)
+    add_instruction("MOD", function(a, b)
+        return a % b
+    end)
+    add_instruction("PRINT", function(a)
+        print(a)
+    end)
+
+    local vm_code = string.format([[
+local %s = {}
+%s[1] = function(a, b) return a + b end
+%s[2] = function(a, b) return a - b end
+%s[3] = function(a, b) return a * b end
+%s[4] = function(a, b) return a / b end
+%s[5] = function(a, b) return a %% b end
+%s[6] = function(a) print(a) end
+]], vm_name, vm_name, vm_name, vm_name, vm_name, vm_name, vm_name)
+
+    local instruction_names = {}
+    for name, id in pairs(instruction_map) do
+        instruction_names[id] = generate_complex_name()
+    end
+
+    for id, name in pairs(instruction_names) do
+        vm_code = vm_code .. string.format("%s[%d] = %s[%d]\n", vm_name, id, vm_name, id)
+    end
+
+    code = code:gsub("([%d%.]+)%s*([%+%-%*%/%%])%s*([%d%.]+)", function(a, op, b)
+        local instruction_name
+        if op == "+" then
+            instruction_name = "ADD"
+        elseif op == "-" then
+            instruction_name = "SUB"
+        elseif op == "*" then
+            instruction_name = "MUL"
+        elseif op == "/" then
+            instruction_name = "DIV"
+        elseif op == "%" then
+            instruction_name = "MOD"
+        end
+        local instruction_id = instruction_map[instruction_name]
+        return string.format("%s[%d](%s, %s)", vm_name, instruction_id, a, b)
+    end)
+
+    code = code:gsub('print%s*%(%s*(.-)%s*%)', function(content)
+        return string.format("%s[%d](%s)", vm_name, instruction_map["PRINT"], content)
+    end)
+
+    return vm_code .. code
+end
 
 function obfuscator.obfuscate_code(code)
     math.randomseed(os.time())
@@ -153,13 +274,14 @@ function obfuscator.obfuscate_code(code)
     obfuscated_code = encrypt_arithmetic(obfuscated_code)
     obfuscated_code = encrypt_function_calls(obfuscated_code)
     obfuscated_code = add_junk_code(obfuscated_code)
+    obfuscated_code = virtualize_code(obfuscated_code)
 
     local wrapper_name = generate_complex_name()
     obfuscated_code = string.format([[local %s=(function() %s end)()
 ]], wrapper_name, obfuscated_code)
 
     obfuscated_code = obfuscated_code:gsub("%s+", " "):gsub("\n", "")
-    
+
     local junk_var_final = generate_complex_name()
     local junk_value_final = math.random(100000, 999999)
     obfuscated_code = string.format("local %s = %d; %s", junk_var_final, junk_value_final, obfuscated_code)
